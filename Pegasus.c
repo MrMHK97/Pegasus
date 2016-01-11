e23030babe8bdc9408d07a45a028306741687564
Muntadhar Haydar <mr.mhk.97@live.com>
2016-01-11 22:18:23 +0300

    V1.0.1

--- Pegasus.c
+++ Pegasus.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <conio.h>
 
 int NumberofEquations;
 double Matrix[10][10], EquationsResults[10], Determinants[11];
@@ -7,39 +8,42 @@ double DeterminantDeterminer(double matrix[10][10]);
 
 main()
 {
-	printf("Pegasus V1.0\nMuntadhar Haydar\n----------------------\n");
+	int i, j, k, x, y;
+	printf("Pegasus V1.0.1\nMuntadhar Haydar aka mrmhk97\n----------------------\n");
 	printf("Enter the number of equations to solve (maximum value is 10): "); scanf("%d", &NumberofEquations);
-	if (NumberofEquations < 2)
+	if (NumberofEquations <= 2 || NumberofEquations > 10)
 	{
 		printf("Sorry! wrong input\n");
+		getch();
 		return 0;
 	}
-	for (int i = 0; i < NumberofEquations; i++)
+	for (i = 0; i < NumberofEquations; i++)
 	{
-		for (int j = 0; j < NumberofEquations; j++)
+		for (j = 0; j < NumberofEquations; j++)
 		{
 			printf("Please, enter the %s parameter of the %s equation: ", NumberOrder(j + 1), NumberOrder(i + 1));
 			scanf("%lf", &Matrix[i][j]);
 		}
 	}
-	for (int k = 0; k < NumberofEquations; k++)
+	for (k = 0; k < NumberofEquations; k++)
 	{
 		printf("Please, enter the %s equation's result: ", NumberOrder(k + 1));
 		scanf("%lf", &EquationsResults[k]);
 	}
 	Determinants[0] = DeterminantDeterminer(Matrix);
-	for (int i = 0; i < NumberofEquations; i++)
+	for (i = 0; i < NumberofEquations; i++)
 	{
 		double newMatrix[10][10];
-		for (int x = 0; x < NumberofEquations; x++)
-			for (int y = 0; y < NumberofEquations; y++)
+		for (x = 0; x < NumberofEquations; x++)
+			for (y = 0; y < NumberofEquations; y++)
 				newMatrix[x][y] = Matrix[x][y];
-		for (int j = 0; j < NumberofEquations; j++)
+		for (j = 0; j < NumberofEquations; j++)
 			newMatrix[j][i] = EquationsResults[j];
 		Determinants[i + 1] = DeterminantDeterminer(newMatrix);
 	}
-	for (int i = 1; i <= NumberofEquations; i++)
-		printf("The value of the %s variable is: %.00lf\n", NumberOrder(i), Determinants[i]);
+	for (i = 1; i <= NumberofEquations; i++)
+		printf("The value of the %s variable is: %.2lf\n", NumberOrder(i), Determinants[i]);
+	getch();
 	return 0;
 }
 
@@ -57,27 +61,28 @@ const char * NumberOrder(int number)
 
 double DeterminantDeterminer(double matrix[10][10])
 {
+	int q;
 	double result = 0;
-	for (int i = 0; i < NumberofEquations; i++)
+	for (q = 0; q < NumberofEquations; q++)
 	{
 		int m = 1, n = 0;
 		double right = 1, left = 1;
 		while (m < NumberofEquations)
 		{
-			if (n == i) { n++; continue; }
+			if (n == q) { n++; continue; }
 			right *= matrix[m][n];
 			m++; n++;
 		}
 		n = 0; m = NumberofEquations - 1;
 		while (n < NumberofEquations)
 		{
-			if (n == i) { n++; continue; }
+			if (n == q) { n++; continue; }
 			left *= matrix[m][n];
 			n++; m--;
 		}
 		double cross = right - left;
-		cross *= ((i % 2) == 0) ? (1) : (-1);
-		cross *= matrix[0][i];
+		cross *= ((q % 2) == 0) ? (1) : (-1);
+		cross *= matrix[0][q];
 		result += cross;
 	}
 	return result;
